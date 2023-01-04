8cbe31449d72971eb7a14ce2538969b0573a03bd
yoon <homer_j@hanmail.net>
2023-01-04 15:31:43 +0900

    useEffect를 통한 체크박스 상태에 따른 데이터 다르게 뿌려주기
    useEffect의 두번째 인자 디펜던시 배열 안에 checked 변수를 넣어 그 부분의 상태가 변경할 때마다 fetch가 새롭게 된다.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,23 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function Products() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
-    fetch('data/products.json')
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+        console.log("🔥뜨끈한 데이터를 네트워크에서 받아옴");
         setProducts(data);
       });
     return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
+      console.log("🧹 깨끗하게 청소하는 일들을 합니다.");
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input
+        id="checkbox"
+        value={checked}
+        type="checkbox"
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox"> Show Only 🔥🔥 핫 세일 </label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
