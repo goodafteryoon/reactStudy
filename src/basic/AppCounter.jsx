5609911072abce126e14e79be696d28acb4bbee6
yoon <homer_j@hanmail.net>
2023-01-06 16:06:05 +0900

    브라우저 객체가 커서를 따라다니면서 이동
    onPointerMouse 속성 사용
    해당 객체에선 스타일 속성

--- src/basic/AppCounter.jsx
+++ src/basic/AppCounter.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Counter from "./components/Counter";
+import Counter from "../components/Counter";
 import "./App.css";
 
 export default function AppCounter() {
