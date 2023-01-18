b2cba117346827da3f4be24924e10c8022cab278
yunna <homer_j@hanmail.net>
2023-01-19 02:53:15 +0900

    [5.14] form 요소 하나의 state로 리팩토링
    연관된 상태값이라면 하나의 객체로 관리할 수 있다.
    email과 name을 form 으로 묶어 객체로 관리한다.

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
 export default function AppForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [form, setForm] = useState({ name: "", email: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault(); // preventDefault()를 하지 않으면 리렌더링됨
+    console.log(form);
+  };
+
+  const handleChange = (e) => {
+    // 인풋에 변경이 발생하게 되면, target에 있는 이름과 target에 있는 밸류를  e.target에서 받아온다
+    const { name, value } = e.target;
+    // 기존에 form에 있는 데이터는 그대로 유지하고, 현재 input이 변경되고 있는 이름의 키에 지금 전달받은 value를 덮어 씌워준다.
+    setForm({ ...form, [name]: value });
   };
 
   return (
@@ -16,20 +23,16 @@ export default function AppForm() {
           type="text"
           id="name"
           name="name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
+          value={form.name}
+          onChange={handleChange}
         />
         <label htmlFor="email">이메일:</label>
         <input
           type="email"
           id="email"
           name="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
+          value={form.email}
+          onChange={handleChange}
         />
         <button>Submit</button>
       </form>
